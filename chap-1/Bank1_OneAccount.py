7be7ede793d42288cbbf643c49d8dc8195cf5aaa
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2026-05-22 21:58:44 -0300

    Finished the first version of the bank account

--- chap-1/Bank1_OneAccount.py
+++ chap-1/Bank1_OneAccount.py
@@ -18,4 +18,49 @@ while True:
     action = action.lower() #force lowercase 
     action = action[0] #first letter 
 
+    if action == 'b':
+        print('Get Balance')
+        userPass = input('Please enter the password: ')
+        if userPass != accountPwd:
+            print('Incorrect password')
+        else:
+            print('Your Balance is:',accountBalance)
+    elif action == 'd':
+        print('Deposit:')
+        userDepositAmount = input('Please enter the amount you want to deposit: ')
+        userDepositAmount = int(userDepositAmount)
+        userPass = input('Please enter the password: ')
+        if userDepositAmount < 0:
+            print('You cannot deposit a negative ammount!')
+        elif userPass != accountPwd:
+            print('Incorrect password')
+        else:
+            accountBalance = accountBalance + userDepositAmount
+            print('Your new balance is:',accountBalance)
 
+    elif action == 's':
+        print('Show:')
+        print('         Name:',accountName)
+        print('         Balance:',accountBalance)
+        print('         Password:',accountPwd)
+    elif action == 'q':
+        break 
+    elif action == 'w':
+        print('Withdraw:')
+
+        userWithdrawlAmount = input('Please enter the amount to withdraw: ')
+        userWithdrawlAmount = int(userWithdrawlAmount)
+        userPass = input('Please enter the password: ')
+        
+        if userWithdrawlAmount < 0:
+            print('You cant withdraw a negative number')
+        elif userPass != accountPwd:
+            print('Incorrect password for this account')
+        elif userWithdrawlAmount > accountBalance:
+            print('You cannot withdraw more than you have in your account')
+
+        else:
+            accountBalance -= userWithdrawlAmount 
+            print('Your new balance is:',accountBalance)
+
+print('done')
